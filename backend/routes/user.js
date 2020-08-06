a8b321b85033457059d0b35460c91b6fff342574
Haroonansari <haroonj.ansari123@gmail.com>
2020-08-06 23:27:20 +0530

    jwt

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,12 @@ const express=require("express");
 const router=express.Router();
 const User=require("../modal/user");
 const bcrypt=require("bcrypt");
+const { route } = require("./posts");
+const jwt=require("jsonwebtoken");
+
+
 
 router.post("/signup",(req,res,next)=>{
-
 bcrypt.hash(req.body.password,10)
 .then(hash=>{
    const user=new User({
@@ -26,9 +29,48 @@ bcrypt.hash(req.body.password,10)
       })
 })
 const user=new User({
-      email:req.body.email,
-      password:req.body.password
+  email:req.body.email,
+  password:req.body.password
 });
 });
+router.post("/login",(req,res,next)=>{
+  let fetchedUser;
+
+  User.findOne({email:req.body.email}).then(user=>{
+    if(!user)
+      {
+        return res.status(401).json({
+          message:"Auth Failed"
+      });
+      }
+      fetchedUser=user;
+      console.log("user"+user);
+      console.log("fethceduser"+fetchedUser)
+      return bcrypt.compare(req.body.password,user.password);
+  })
+  .then(result=>
+    {
+      if(!result)
+      {
+        return res.status(401).json({
+          message:"Auth Failed"
+        });
+      }
+
+    console.log("ddddd"+fetchedUser)
+    const token=jwt.sign({email:fetchedUser.email,userId:fetchedUser._id},
+      "secretly-created-json-token",{expiresIn:"1h"});
+      res.status(201).json({
+        token:token
+      })
+    })
+    .catch(err=>
+      {
+        return res.status(401).json({
+          message:"Auth Failed"
+        })
+      })
+})
+
 module.exports=router;
 
