3aff7085e07a81093ef0e0d24d05695a67d74223
Haroon <haroona@ilink-systems.com>
2020-08-11 00:27:38 +0530

    error-1

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-error',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ErrorComponent implements OnInit {
 
   message="Error occured"
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data:{message:string}) { }
 
   ngOnInit() {
   }
